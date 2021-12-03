e9bae8140bf4d8fd7553111f7654daf11e696332
Þór Breki <thorthorgrims@gmail.com>
2021-12-03 13:25:20 +0000

    Update LLAPI.py

--- NaNair39/Logic_layer/LLAPI.py
+++ NaNair39/Logic_layer/LLAPI.py
@@ -1,6 +1,7 @@
 from Logic_layer.ChangeHandler import ChangeHandler
 
 class LLAPI():
-    def __init__(self) -> None:
+    def __init__(self, data_API) -> None:
         change_handler = ChangeHandler()
+        dl_API = data_API
     
