3675d833e2c852cc2bb700f333078e626aff0daf
Ingik21 <89972195+Ingik21@users.noreply.github.com>
2021-12-03 11:15:21 +0000

    Update PropertiesDL.py

--- NaNair39/Data_layer/PropertiesDL.py
+++ NaNair39/Data_layer/PropertiesDL.py
@@ -7,7 +7,7 @@ class PropertyDL:
     def __init__(self):
         self.filepath = "csv_files/Properties.csv"
 
-    def get_all_employees(self):
+    def get_all_properties(self):
         return_list = []
         with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile)
@@ -16,14 +16,14 @@ class PropertyDL:
                 return_list.append(prop)
         return return_list
     
-    def create_employee(self, prop):
+    def create_property(self, prop):
         with open(self.filepath, 'a', newline='', encoding='utf-8') as csvfile:
             fieldnames = ["idnumber","name","location","address","size","rooms"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({'Property': prop.idnumber, 'name': prop.name, 'location': prop.location, 'address': prop.address, 'size': prop.size, 'no of rooms': prop.rooms})
     
-    def delete_employee(self, prop):
+    def delete_property(self, prop):
         pass
 
-    def change_information(self, prop):
+    def change_information_property(self, prop):
         pass
