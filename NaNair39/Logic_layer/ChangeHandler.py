bfaf8881e6b5e886a08aab89ad8e4e8dc02542d5
Þór Breki <thorthorgrims@gmail.com>
2021-12-03 10:50:56 +0000

    Update ChangeHandler.py

--- NaNair39/Logic_layer/ChangeHandler.py
+++ NaNair39/Logic_layer/ChangeHandler.py
@@ -1,4 +1,6 @@
+from typing import Sized
 from Models.EmployeeModel import Employee
+from Models.PropertyModel import Property
 from Logic_layer.LLAPI import LLAPI
 
 NAME = "name"
@@ -7,6 +9,9 @@ ADDRESS = "address"
 PHONE = "phone"
 CELLPHONE = "cellphone"
 TITLE = "title"
+
+SIZE = "size"
+ROOMS = "rooms"
 
 class ChangeHandler():
     def __init__(self) -> None:
@@ -27,11 +32,20 @@ class ChangeHandler():
         elif attribute == TITLE:
             employee.title = new_value
 
-    def change_property(property:Property, attribute, new_value):
+    def change_property(property:Property, attribute:str, new_value:str):
+        attribute = attribute.lower()
+        if attribute == NAME:
+            property.name = new_value
+        if attribute == LOCATION:
+            property.location = new_value
+        if attribute == SIZE:
+            property.size = new_value
+        if attribute == ROOMS:
+            property.rooms = int(new_value)
+
+
+    def change_contractor(contractor:Contractor, attribute:str, new_value:str):
         pass
 
-    def change_contractor(contractor:Contractor, attribute, new_value):
-        pass
-
-    def change_work_order(work_order:WorkOrder, attribute, new_value):
+    def change_work_order(work_order:WorkOrder, attribute:str, new_value:str):
         pass
