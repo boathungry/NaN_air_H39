8d86d2ea7cd68c6493c9306dff996cc2d5204630
Hjölli <ezithau@gmail.com>
2021-12-03 13:34:42 +0000

    ok

---
+++ NaNair39/Data_layer/WorkordersDL.py
@@ -0,0 +1,29 @@
+import csv
+
+from Models.WorkorderModel import Workorder
+
+class WorkorderDL:
+
+    def __init__ (self):
+        self.filepath = "csv_files/Workorders.csv"
+
+    def get_all_Workorders(self):
+        return_list = []
+        with open(self.filepath, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                VB = Workorder(row["work request"],row["location"], row["properties"], row["description"])
+                return_list.append(VB)
+        return return_list
+    
+    def create_workorder(self, VB):
+        with open(self.filepath, 'a', newline='', encoding='utf-8') as csvfile:
+            fieldnames = ["work request","location","properties","description"]
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'': VB.workorder, 'location': VB.location, 'property': VB.properties, 'description': VB.description})
+    
+    def delete_workorder(self, VB):
+        pass
+
+    def change_information_workorder(self, VB):
+        pass
