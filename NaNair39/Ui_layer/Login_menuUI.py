af10db43f522f2299c8a4ea80fc5a31449c9e612
Hjölli <ezithau@gmail.com>
2021-12-06 18:21:07 +0000

    ok

--- NaNair39/Ui_layer/Login_menuUI.py
+++ NaNair39/Ui_layer/Login_menuUI.py
@@ -2,8 +2,9 @@ from Data_layer.EmployeeDL import EmployeeDL
 class LogIn:
     def innskra():
         email = input("Please input your email: ")
+        print(email)
         user = EmployeeDL.search_by_email(email)
         return user
-        
+
         
         
