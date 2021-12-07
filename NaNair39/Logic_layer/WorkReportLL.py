c0c9d05866020f40000c470539feda32583a34ee
valdisgunngeirs <89972358+valdisgunngeirs@users.noreply.github.com>
2021-12-07 14:46:36 +0000

    ok

--- NaNair39/Logic_layer/WorkReportLL.py
+++ NaNair39/Logic_layer/WorkReportLL.py
@@ -1,7 +1,12 @@
+from Models.WorkReportModel import WorkReport
+from Logic_layer.LLAPI import LLAPI
+from ChangeHandler import ChangeHandler
+from datetime import date
+
 class WorkReport():
 
     def __init__(self) -> None:
         pass
 
-    def approve_report():
-        
+    def approve_report(self, work_report:WorkReport):
+        ChangeHandler.change_work_report(self, work_report, "approved", date.today())
