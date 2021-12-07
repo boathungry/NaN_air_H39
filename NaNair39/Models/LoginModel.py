e23485cdcfecff942ebb6f18e6de9bf9ea22dd82
Ingik21 <89972195+Ingik21@users.noreply.github.com>
2021-12-07 08:28:35 +0000

    Create LoginModel.py

---
+++ NaNair39/Models/LoginModel.py
@@ -0,0 +1,9 @@
+class LoginAccount:
+    def __init__(self, name, email, location, title):
+        self.name = name
+        self.email = email
+        self.location = location
+        self.title = title
+
+    def __str__(self):
+        return f"Name: {self.name}, email: {self.email}, location: {self.location}, job title {self.title}"
