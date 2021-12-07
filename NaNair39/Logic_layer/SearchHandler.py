dca2ea299f50124e7b675ad440a72ee8779a9781
Þór Breki <thorthorgrims@gmail.com>
2021-12-07 13:37:47 +0000

    Update SearchHandler.py

--- NaNair39/Logic_layer/SearchHandler.py
+++ NaNair39/Logic_layer/SearchHandler.py
@@ -1,6 +1,21 @@
+from Logic_layer.LLAPI import LLAPI
+from Data_layer.DLAPI import DLAPI
+from Models.EmployeeModel import Employee
+from Models.PropertyModel import Property
+from Models.WorkReportModel import WorkReport
+
+
 class SearchHandler():
     def __init__(self) -> None:
         pass
 
     def search(self, object, attribute, value):
+        pass
+
+    def search_for(self, object):
+        """Determines whether the object being searched for is an employee, property, etc. and returns the object type."""
+        pass
+
+    def search_for_empl_by(self, attribute, value):
+        """Searches for employees whose given attribute (name, email, ID, etc.) matches the given value."""
         pass
