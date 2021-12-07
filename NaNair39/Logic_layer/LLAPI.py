1fd77fe6aaf0e40e25192dd5361f3b6796660614
Þór Breki <thorthorgrims@gmail.com>
2021-12-07 22:21:51 +0000

    Fixed some comments on the LLAPI

--- NaNair39/Logic_layer/LLAPI.py
+++ NaNair39/Logic_layer/LLAPI.py
@@ -16,36 +16,41 @@ class LLAPI():
         self.work_request = WorkRequest(data_API)
     
     def change_employee(self, employee, attribute, new_value):
-        """Passes the chosen employee, attribute and new value through the logic layer where the attribute is changed to the new value."""
-        self.change_handler.change_employee(employee, attribute, new_value)
+        """Changes an attribute of an employee to a new value. Returns the given attribute if successful, otherwise returns None."""
+        return self.change_handler.change_employee(employee, attribute, new_value)
 
     def change_property(self, property, attribute, new_value):
-        """Passes the chosen property, attribute and new value through the logic layer where the attribute is changed to the new value."""
-        self.change_handler.change_property(property, attribute, new_value)
+        """Changes an attribute of a property to a new value. Returns the given attribute if successful, otherwise returns None."""
+        return self.change_handler.change_property(property, attribute, new_value)
 
     def change_contractor(self, contractor, attribute, new_value):
-        """Passes the chosen contractor, attribute and new value through the logic layer where the attribute is changed to the new value."""
-        self.change_handler.change_contractor(contractor, attribute, new_value)
+        """Changes an attribute of a contractor to a new value. Returns the given attribute if successful, otherwise returns None."""
+        return self.change_handler.change_contractor(contractor, attribute, new_value)
 
     def change_work_report(self, work_report, attribute, new_value):
-        """Passes the chosen work report, attribute and new value through the logic layer where the attribute is changed to the new value."""
-        self.change_handler.change_work_report(work_report, attribute, new_value)
+        """Changes an attribute of a work report to a new value. Returns the given attribute if successful, otherwise returns None."""
+        return self.change_handler.change_work_report(work_report, attribute, new_value)
 
     def change_work_request(self, work_request, attribute, new_value):
-        """Passes the chosen work request, attribute and new value through the logic layer where the attribute is changed to the new value."""
-        self.change_handler.change_work_request(work_request, attribute, new_value)
+        """Changes an attribute of a work request to a new value. Returns the given attribute if successful, otherwise returns None."""
+        return self.change_handler.change_work_request(work_request, attribute, new_value)
 
     def create_employee(self, name, location, address, phone, cellphone, title):
-        self.registration_handler.register_employee(name, location, address, phone, cellphone, title)
+        """Creates a new employee with the given attributes and returns the employee."""
+        return self.registration_handler.register_employee(name, location, address, phone, cellphone, title)
 
     def create_property(self, name, location, size, rooms):
-        self.registration_handler.register_property(name, location, size, rooms)
+        """Creates a new property with the given attributes and returns the property."""
+        return self.registration_handler.register_property(name, location, size, rooms)
     
     def create_contractor(self, name, phone, email, opening_hours, address):
-        self.registration_handler.register_contractor(name, phone, email, opening_hours, address)
+        """Creates a new contractor with the given attributes and returns the contractor."""
+        return self.registration_handler.register_contractor(name, phone, email, opening_hours, address)
 
     def create_work_report(self, id, work_request_id, description, location, properties, worker, comment, regular_maintenance, expenses, start, done, approved):
-        self.registration_handler.register_work_report(id, work_request_id, description, location, properties, worker, comment, regular_maintenance, expenses, start, done, approved)
+        """Creates a new work report with the given attributes and returns the work report."""
+        return self.registration_handler.register_work_report(id, work_request_id, description, location, properties, worker, comment, regular_maintenance, expenses, start, done, approved)
 
     def create_work_request(self, id, work_request, location, properties, description, worker, priority, repeat, time, start, done):
-        self.registration_handler.register_work_request(id, work_request, location, properties, description, worker, priority, repeat, time, start, done)
+        """Creates a new work request with the given attributes and returns the work request."""
+        return self.registration_handler.register_work_request(id, work_request, location, properties, description, worker, priority, repeat, time, start, done)
