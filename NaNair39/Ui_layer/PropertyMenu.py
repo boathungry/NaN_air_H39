8b6b3b4babc199a0d22880d1e947b96724e4fd0b
Hjölli <ezithau@gmail.com>
2021-12-07 12:27:21 +0000

    ok

--- NaNair39/Ui_layer/PropertyMenu.py
+++ NaNair39/Ui_layer/PropertyMenu.py
@@ -1,71 +1,77 @@
-import Ui_layer.MainMenuEMPUI
+import Ui_layer.MainMenuMANUI
 class PropertyMenu:
-    def __init__(self):
+    def __init__(self, title):
         super().__init__()
 
     def location_options_mangers(self):
-        location_deets = "get_location_details(self.location)"
-        manpropon = True
-        while manpropon:
-            print("1. Destinations")
-            print("2. Properties")
-            print("b. Back")
-            print("q. Quit")
-            selection = input("Input selection")
-            if selection == "1":
-                self.destination_manager_menu()
-            elif selection == "2":
-                self.property_manager_menu()
-            elif selection.lower() == "b":
-                pass
-            elif selection.lower() == "q":
-                manpropon = False
-            else:
-                print("Wrong input.")
+        """location_deets = "get_location_details(self.location)"""
+        
+        print("1. Destinations")
+        print("2. Properties")
+        print("b. Back")
+        print("q. Quit")
+        selection = input("Input selection: ")
+        if selection == "1":
+            self.destination_manager_menu()
+        elif selection == "2":
+            self.property_manager_menu()
+        elif selection.lower() == "b":
+            user = Ui_layer.MainMenuMANUI.ManagerUI("manager")
+            Ui_layer.MainMenuMANUI.ManagerUI.managers_menu(user)
+        elif selection.lower() == "q":
+            pass
+        else:
+            print("Wrong input.")
+            self.location_options_mangers()
 
-    def destination_manager_menu():
-        manlocon = True
-        while manlocon:
-            print("1. Register new destination")
-            print("2. Change existin destination")
-            print("3. Get list of existing destination")
-            print("4. Search for destination")
-            print("b. Back to main menu")
-            print("q. Quit")
-            selection = input("Input selection: ")
-            if selection == "1":
-                pass
-            elif selection == "2":
-                pass
-            elif selection == "3":
-                pass
-            elif selection == "4":
-                pass
-            elif selection.lower() == "b":
-                Ui_layer.MainMenuEMPUI.ManagerUI.managers_menu()
-            elif selection.lower() == "q":
-                manlocon = False
-    def property_manager_menu():
-        propmanmenuon = True
-        while propmanmenuon:
-            print("1. Create new property")
-            print("2. Edit existing property")
-            print("3. Get list of properties")
-            print("4. Look up property")
-            print("b. back to main menu")
-            print("q. quit")
-            selection = input("Input selection: ")
-            if selection == "1":
-                pass
-            elif selection == "2":
-                pass
-            elif selection == "3":
-                pass
-            elif selection == "4":
-                pass
-            elif selection.lower() == "b":
-                Ui_layer.MainMenuEMPUI.ManagerUI.managers_menu()
-            elif selection.lower() == "q":
-                propmanmenuon = False
-            else:
-                print("Wrong input")
+
+    def destination_manager_menu(self):
+        print("1. Register new destination")
+        print("2. Change existin destination")
+        print("3. Get list of existing destination")
+        print("4. Search for destination")
+        print("b. Back to main menu")
+        print("q. Quit")
+        selection = input("Input selection: ")
+        if selection == "1":
+            pass
+        elif selection == "2":
+            pass
+        elif selection == "3":
+            pass
+        elif selection == "4":
+            pass
+        elif selection.lower() == "b":
+            user = Ui_layer.MainMenuMANUI.ManagerUI("manager")
+            Ui_layer.MainMenuMANUI.ManagerUI.managers_menu(user)
+        elif selection.lower() == "q":
+            pass
+        else:
+            print("Wrong input!")
+            self.destination_manager_menu()
+    
+    def property_manager_menu(self):
+        
+        print("1. Create new property")
+        print("2. Edit existing property")
+        print("3. Get list of properties")
+        print("4. Look up property")
+        print("b. back to main menu")
+        print("q. quit")
+        selection = input("Input selection: ")
+        if selection == "1":
+            pass
+        elif selection == "2":
+            pass
+        elif selection == "3":
+            pass
+        elif selection == "4":
+            pass
+        elif selection.lower() == "b":
+            user = Ui_layer.MainMenuMANUI.ManagerUI("manager")
+            Ui_layer.MainMenuMANUI.ManagerUI.managers_menu(user)
+        elif selection.lower() == "q":
+            pass
+        else:
+            print("Wrong input")
+            self.property_manager_menu()
