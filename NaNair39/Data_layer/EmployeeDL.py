e394ea3cce4b7e8c98a9e4197a873ba8dffae250
Ingik21 <ingik21@ru.is>
2021-12-08 10:05:29 +0000

    ok

--- NaNair39/Data_layer/EmployeeDL.py
+++ NaNair39/Data_layer/EmployeeDL.py
@@ -1,5 +1,4 @@
 import csv
-from os import name
 from Models.LoginModel import LoginAccount
 from Models.EmployeeModel import Employee
 
@@ -19,7 +18,7 @@ class EmployeeDL:
     def get_all_employees(self):
         '''Returns a list of employees in the given filepath'''
         return_list = []
-        with open("/Users/valdisosk/Desktop/csvtest/Employee.csv", newline='', encoding='utf-8') as csvfile:
+        with open(self.filepath, newline='', encoding='utf-8') as csvfile:
             reader = csv.DictReader(csvfile, delimiter=',')
             for row in reader:
                 print(row["name"], "-", row["email"], "-", row["location"], "-", row["address"], "-", row["phone"], "-", row["cellphone"], "-", row["title"])
