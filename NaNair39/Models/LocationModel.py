fb6395269f162521dd2e5589f6fc27469ed7c5e4
Ingik21 <ingik21@ru.is>
2021-12-08 12:50:22 +0000

    OK

--- NaNair39/Models/LocationModel.py
+++ NaNair39/Models/LocationModel.py
@@ -1,8 +1,11 @@
 class Location:
-    def __init__(self, city, country):
+    def __init__(self, city, country, airport, phone, opening, manager):
         self.city = city
         self.country = country
-
+        self.airport = airport
+        self.phone = phone
+        self.opening = opening
+        self.manager = manager
 
     def __str__(self):
-        return f"City: {self.city}, Country: {self.country}"
+        return f"City: {self.city}, Country: {self.country}, Local airport: {self.airport}, Branch phone number: {self.phone}, Opening hours: {self.opening}, Local manager: {self.manager}"
