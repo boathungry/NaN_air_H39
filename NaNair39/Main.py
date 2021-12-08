2de369bf93136f58b45a20aacb4cac88a9faeb11
Ingik21 <ingik21@ru.is>
2021-12-08 12:00:10 +0000

    ok

--- NaNair39/Main.py
+++ NaNair39/Main.py
@@ -9,26 +9,20 @@ def main():
     Ui_layer.main_login.LogedIn.main_menu(user)
     """selection = Ui_layer.Login_menuUI.LogIn.temp_innskra()
     if selection == "1":
-<<<<<<< HEAD
         user = ManagerUI("Manager")
         ManagerUI.managers_menu(user)
     elif selection == "2":
         Ui_layer.MainMenuEMPUI
     elif selection.lower == "q":
-=======
         Ui_layer.MainMenuMANUI.ManagerUI(title="manager").managers_menu()
         
     elif selection == "2":
         Ui_layer.MainMenuEMPUI.EmployeeUI(title="staff").staff_menu()
     elif selection.lower() == "q":
->>>>>>> af08d48a85a77809a569318da6dbcaa9d13547dc
         pass
     else:
         print("Wrong input!")
-<<<<<<< HEAD
         main()
 main()
-=======
         main()"""
 main()
->>>>>>> 2d07f59c42ece422c8a32fd932f0f4ff11c0d2fa
